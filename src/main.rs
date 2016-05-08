2f9ada6f33e7c9c6ca312f626e6bb9a6f872d682
Tristan Hume <tris.hume@gmail.com>
2016-05-08 13:08:14 -0400

    Revert "Running QML"
    This reverts commit 8df919630c7c3603a004f7d091d95f102aee9c79.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,3 @@
-#[macro_use]
-extern crate qmlrs;
-extern crate find_folder;
-
 fn main() {
-    let mut engine = qmlrs::Engine::new();
-
-    // engine.set_property("factorial", math::Factorial);
-
-    let assets = find_folder::Search::KidsThenParents(3, 5)
-                .for_folder("assets").unwrap();
-    let main_file = assets.join("main.qml");
-    engine.load_local_file(main_file.to_str().unwrap());
-
-    engine.exec();
+    println!("Hello, world!");
 }
